838b4bb4ffc2e52f7dab6da59135450e1ea1fb8d
chromaticphdevs <gonzalesmarkangeloph@gmail.com>
2021-12-06 14:02:24 +0800

    up-relase

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ordering_system/';
+$config['base_url'] = 'https://e-kahon.store';
 
 /*
 |--------------------------------------------------------------------------
