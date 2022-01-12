21ffe9f95c3cf65dd85107ca775ea04a4d431f07
chromaticphdevs <gonzalesmarkangeloph@gmail.com>
2022-01-12 09:37:27 +0800

    fix bundle issue

--- application/views/bundle/index.php
+++ application/views/bundle/index.php
@@ -5,6 +5,7 @@
         <div class="container">
             <?php flash()?>
                 <?php foreach($bundles as $key => $row) :?>
+                    <?php $no_stock_item = false?>
                     <?php $price_amount = 0?>
                     <form method="get" action="<?php echo base_url('/cart/checkout?bundle_id='.$row['id'])?>">
                         <input type="hidden" name="bundle_id" value="<?php echo $row['id']?>">
@@ -23,9 +24,17 @@
 
                                     <tbody>
                                         <?php foreach($row['items'] as $itemKey => $itemRow) :?>
+                                            <?php
+                                                $no_stock = false;
+                                                if( $itemRow['stocks'] > $itemRow['min_stock']) {
+                                                    $no_stock_item = true;
+                                                    $no_stock = true;
+                                                }
+                                            ?>
                                             <?php $price_amount += ($itemRow['price'] * $itemRow['quantity'])?>
                                             <tr>
-                                                <td><?php echo $itemRow['name']?></td>
+                                                <td><?php echo $itemRow['name']?> 
+                                                <?php echo $no_stock ? '<span class="text-danger"> No Stock </span>' : '' ?> </td>
                                                 <td><?php echo $itemRow['price']?></td>
                                                 <td><?php echo $itemRow['quantity']?></td>
                                             </tr>
@@ -33,11 +42,15 @@
                                     </tbody>
                                 </table>
 
-                                <h4>Total: <s><?php echo amountHTML($price_amount)?></s> <?php echo amountHTML($price_amount - $row['discount'])?></h4>
+                                <h4>Total: <s><?php echo amountHTML($row['price_public'])?></s> <?php echo amountHTML($row['price_public'] - $row['discount'])?></h4>
                             </div>
 
                             <div class="card-footer">
+                                <?php if(!$no_stock_item) :?>
                                 <input type="submit" name="" value="Checkout" class="btn btn-primary">
+                                <?php else:?>
+                                    <p>No Stock</p>
+                                <?php endif?>
                             </div>
                         </div>
                     </form>
